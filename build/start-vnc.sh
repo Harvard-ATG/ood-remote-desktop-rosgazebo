e22b4448869310340242a28083c8db2e85961b2b
Arthur Barrett <abarrett@fas.harvard.edu>
2024-08-07 10:00:14 -0400

    fix: make sure to kill an existing vncserver session

--- build/start-vnc.sh
+++ build/start-vnc.sh
@@ -3,5 +3,6 @@
 echo 'Updating /etc/hosts file...'
 HOSTNAME=$(hostname)
 echo "127.0.1.1\t$HOSTNAME" >> /etc/hosts
+/usr/bin/vncserver -kill :1 || true
 /usr/bin/vncserver -geometry 1024x768
 tail -f /root/.vnc/*:1.log
